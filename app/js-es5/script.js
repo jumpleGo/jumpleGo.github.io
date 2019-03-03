bb29aeef909c1183baa4a4b362c6e3ef9ed371db
Emil <rrotatew@gmail.com>
2019-03-04 01:18:52 +0300

    #1

--- app/js-es5/script.js
+++ app/js-es5/script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+$(function () {
+  $(".add_btn").on('click', function (e) {
+    var src = $('.img').val();
+    var cryptoname = $('.price').val();
+    var income = $('.name').val();
+    e.preventDefault();
+    $(".orders").append("<div class='info_pack'> <div class='info_pack_img'>" + " <img style='width: 30px;height:30px;'" + " src='images/" + src + " \'>" + "</div><div" + " class='info_pack_text'" + "> <span>" + cryptoname + "</span></div><div" + " class='info_pack_money'> <span> + " + income + "$ </span></div></div>");
+  });
+});
+$('.main-carousel').flickity(_defineProperty({
+  freeScroll: true,
+  contain: true,
+  prevNextButtons: false,
+  pageDots: false,
+  cellAlign: 'left'
+}, "contain", true));
--- app/js-es6/script.js
+++ app/js-es6/script.js
@@ -1,1 +1,26 @@
+$(function () {
 
+    $(".add_btn").on('click', function (e) {
+        let src = $('.img').val();
+        let cryptoname = $('.price').val();
+        let income = $('.name').val();
+        e.preventDefault();
+        $(".orders").append(
+            "<div class='info_pack'> <div class='info_pack_img'>" +
+            " <img style='width: 30px;height:30px;'" +
+            " src='images/" + src + " \'>" + "</div><div" + " class='info_pack_text'" +
+            "> <span>" +
+            cryptoname +
+            "</span></div><div" + " class='info_pack_money'> <span> + " +
+            income +
+            "$ </span></div></div>");
+    })
+});
+$('.main-carousel').flickity({
+    freeScroll: true,
+    contain: true,
+    prevNextButtons: false,
+    pageDots: false,
+    cellAlign: 'left',
+    contain: true
+});
