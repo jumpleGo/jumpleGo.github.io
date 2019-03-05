28ffffb739cb93c3bf1f43cc4dd0ec78a480b078
Emil <rrotatew@gmail.com>
2019-03-05 20:16:43 +0300

    1

--- app/js-es5/script.js
+++ app/js-es5/script.js
@@ -2,15 +2,48 @@
 
 $(function () {
   var src;
+  var cryptoname;
   $(".add_btn").on('click', function (e) {
-    var cryptoname = $('.price').val();
-    var income = $('.name').val();
+    var price1 = $('.price1').val();
+    var price2 = $('.price2').val();
+    var colvo1 = $('.colvo1').val();
+    var colvo2 = $('.colvo2').val();
+    var income = price2 * colvo2 - price1 * colvo1;
     e.preventDefault();
-    $(".orders").append("<div class='info_pack'> <div class='info_pack_img'>" + " <img style='width: 30px;height:30px;'" + " src='" + src + " \'>" + "</div><div" + " class='info_pack_text'" + "> <span>" + cryptoname + "</span></div><div" + " class='info_pack_money'> <span> + " + income + "$ </span></div></div>");
+    $(".orders").append("  <div class='info_pack'> <div class='info_pack_row'>  <div class='info_pack_row_img'>" + " <img style='width: 30px;height:30px;'" + " src='" + src + " \'>" + "</div><div" + " class='info_pack_row_text'" + "> <span>" + cryptoname + "</span></div></div><div" + " class='info_pack_money'> <span>  " + income + "$ </span></div></div>");
   });
   $('img').click(function () {
     src = $(this).attr('src');
-    console.log(src);
+
+    switch (src) {
+      case 'images/LTC.png':
+        cryptoname = "litecoin";
+        break;
+
+      case 'images/BTC.png':
+        cryptoname = "bitcoin";
+        break;
+
+      case 'images/dash.png':
+        cryptoname = "dash";
+        break;
+
+      case 'images/NEO.png':
+        cryptoname = "neo";
+        break;
+
+      case 'images/XRP.png':
+        cryptoname = "ripple";
+        break;
+
+      case 'images/BCH.png':
+        cryptoname = "bitcoin cash ";
+        break;
+
+      case 'images/BNB.png':
+        cryptoname = "binance coin";
+        break;
+    }
   });
 });
 $('.main-carousel').flickity({
--- app/js-es6/script.js
+++ app/js-es6/script.js
@@ -1,24 +1,53 @@
 $(function () {
     let src;
+    let cryptoname;
     $(".add_btn").on('click', function (e) {
 
-        let cryptoname = $('.price').val();
-        let income = $('.name').val();
+        let price1 = $('.price1').val();
+        let price2 = $('.price2').val();
+        let colvo1 = $('.colvo1').val();
+        let colvo2 = $('.colvo2').val();
+
+        let income = (price2 * colvo2) - (price1 * colvo1);
+
         e.preventDefault();
         $(".orders").append(
-            "<div class='info_pack'> <div class='info_pack_img'>" +
+            "  <div class='info_pack'> <div class='info_pack_row'>  <div class='info_pack_row_img'>" +
             " <img style='width: 30px;height:30px;'" +
-            " src='" + src + " \'>" + "</div><div" + " class='info_pack_text'" +
+            " src='" + src + " \'>" + "</div><div" + " class='info_pack_row_text'" +
             "> <span>" +
             cryptoname +
-            "</span></div><div" + " class='info_pack_money'> <span> + " +
+            "</span></div></div><div" + " class='info_pack_money'> <span>  " +
             income +
             "$ </span></div></div>");
 
     });
     $('img').click(function () {
         src = $(this).attr('src');
-        console.log(src);
+        switch (src) {
+            case 'images/LTC.png':
+                cryptoname = "litecoin";
+                break;
+            case 'images/BTC.png':
+                cryptoname = "bitcoin";
+                break;
+            case 'images/dash.png':
+                cryptoname = "dash";
+                break;
+            case 'images/NEO.png':
+                cryptoname = "neo";
+                break;
+            case 'images/XRP.png':
+                cryptoname = "ripple";
+                break;
+            case 'images/BCH.png':
+                cryptoname = "bitcoin cash ";
+                break;
+            case 'images/BNB.png':
+                cryptoname = "binance coin";
+                break;
+
+        }
     });
 });
 $('.main-carousel').flickity({
