40200b2737de280b7604a9b67d4ea1c18dfcd04e
Emil <rrotatew@gmail.com>
2019-03-13 23:24:31 +0300

    2

--- app/js-es5/firebase.js
+++ app/js-es5/firebase.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+(function () {
     var config = {
         apiKey: "AIzaSyCwHDCI2f0j4IFlPGJPwavi6e-xGAYeNiM",
         authDomain: "crypto782.firebaseapp.com",
@@ -10,19 +10,74 @@ $(document).ready(function () {
     firebase.initializeApp(config);
     var firestore = firebase.firestore();
 
-    const docRef = firestore.doc('values/9fIV2BbJnUrcMmtmSOw3');
-
+    const docRef = firestore.doc('values/9fIV2BbJnUrcMmtmSOw3/collection/ehZezTz7fuzw9rWygN7z');
+    const sourse = document.querySelector('.carousel-cell-img').getAttribute('src');
     const button = document.querySelector('.buttons_right');
 
 
-    button.addEventListener("click", function () {
+    button.addEventListener("click", function name() {
+        let cryptoname;
         const price1 = document.querySelector('.price1').value;
         const price2 = document.querySelector('.price2').value;
         const total1 = document.querySelector('.colvo1').value;
         const total2 = document.querySelector('.colvo2').value;
-        console.log(price1);
+        let income = (total2 * price2) - (price1 * total1);
+        switch (sourse) {
+            case 'images/LTC.png':
+                cryptoname = "litecoin";
+                break;
+            case 'images/BTC.png':
+                cryptoname = "bitcoin";
+                break;
+            case 'images/dash.png':
+                cryptoname = "dash";
+                break;
+            case 'images/NEO.png':
+                cryptoname = "neo";
+                break;
+            case 'images/XRP.png':
+                cryptoname = "ripple";
+                break;
+            case 'images/BCH.png':
+                cryptoname = "bitcoin cash ";
+                break;
+            case 'images/BNB.png':
+                cryptoname = "binance coin";
+                break;
+
+        }
         docRef.set({
-            pr: price1
+            income: income,
+            priceBuy: price1,
+            priceCell: price2,
+            totalBuy: total1,
+            totalCell: total2,
+            src: sourse,
+            cryptoname: cryptoname
         });
+
+        $('.modal').removeClass('active');
+        getRealTimeUpdates();
     });
+    getRealTimeUpdates = function () {
+        docRef.onSnapshot(function (doc) {
+            if (doc && doc.exists) {
+                const myData = doc.data();
+
+                document.querySelector(".orders").innerHTML =
+                    "  <div class='info_pack'> <div class='info_pack_row'>  <div class='info_pack_row_img'>" +
+                    " <img style='width: 30px;height:30px;'" +
+                    " src='" + myData.src + " \'>" + "</div><div" + " class='info_pack_row_text'" +
+                    "> <span>" +
+                    myData.cryptoname +
+                    "</span></div></div><div" + " class='info_pack_money'> <span>  " +
+                    myData.income +
+                    "$ </span></div></div>";
+            }
+        });
+    }
+
+
+
+
 }());
--- app/js-es5/script.js
+++ app/js-es5/script.js
@@ -1,58 +1,9 @@
 "use strict";
 
-$(function () {
-    var src;
-    var cryptoname;
-    $(".buttons_right").on('click', function (e) {
-        var price1 = $('.price1').val();
-        var price2 = $('.price2').val();
-        var colvo1 = $('.colvo1').val();
-        var colvo2 = $('.colvo2').val();
-        var income = price2 * colvo2 - price1 * colvo1;
-        e.preventDefault();
-        $(".orders").append("  <div class='info_pack'> <div class='info_pack_row'>  <div class='info_pack_row_img'>" + " <img style='width: 30px;height:30px;'" + " src='" + src + " \'>" + "</div><div" + " class='info_pack_row_text'" + "> <span>" + cryptoname + "</span></div></div><div" + " class='info_pack_money'> <span>  " + income + "$ </span></div></div>");
-        $('.modal').removeClass('active');
-        e.preventDefault();
-    });
-    $('img').click(function () {
-        src = $(this).attr('src');
-
-
-        switch (src) {
-            case 'images/LTC.png':
-                cryptoname = "litecoin";
-                break;
-
-            case 'images/BTC.png':
-                cryptoname = "bitcoin";
-                break;
-
-            case 'images/dash.png':
-                cryptoname = "dash";
-                break;
-
-            case 'images/NEO.png':
-                cryptoname = "neo";
-                break;
-
-            case 'images/XRP.png':
-                cryptoname = "ripple";
-                break;
-
-            case 'images/BCH.png':
-                cryptoname = "bitcoin cash ";
-                break;
-
-            case 'images/BNB.png':
-                cryptoname = "binance coin";
-                break;
-        }
-    });
-});
 $('.add_btn').click(function (e) {
-    $('.modal').addClass('active');
-    e.preventDefault();
+  $('.modal').addClass('active');
+  e.preventDefault();
 });
 $('.buttons_left').click(function (e) {
-    $('.modal').removeClass('active');
+  $('.modal').removeClass('active');
 });
